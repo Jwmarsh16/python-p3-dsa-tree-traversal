9338a5b6e3ee6b6a1976a55eeccc9c93c0d4241d
Jonathan Marshall <jwmarsh16@gmail.com>
2024-07-10 12:51:25 -0500

    lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        def dfs(node):
+            if node['id'] == id:
+                return node
+            for child in node.get('children', []):
+                result = dfs(child)
+                if result:
+                    return result
+            return None
+
+        if self.root is None:
+            return None
+        return dfs(self.root)
